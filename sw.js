d9e4fc010974b3a83bc77459ca41cb3c8da0bf9a
KhushalSiddhu <khushalsiddhu27@gmail.com>
2026-08-09 08:37:00 +0530

    Add files via upload

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='gamevault-v2';const A=['./','./index.html','./game.html','./about.html','./style.css','./app.js','./games.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
